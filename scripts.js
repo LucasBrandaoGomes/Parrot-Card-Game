818566cde5338be77b603ebc597ef5860c5b8926
brandslucas@gmail.com <brandslucas@gmail.com>
2022-04-07 14:07:20 -0300

    Embaralhar não está funcionando

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
-alert("BEM VINDO AO PARROT CARD GAMES - JOGO DA MEMÓRIA");
-let jogo = prompt("Com quantas cartas deseja jogar?");
+alert("Bem Vinde ao PARROT CARD GAMES - JOGO DA MEMÓRIA");
+let jogo = Number(prompt("Com quantas cartas deseja jogar? Escolha um número par entre 4 e 14 cartas"));
+while ((jogo>14) || (jogo%2 !== 0)){
+        jogo = Number(prompt("Com quantas cartas deseja jogar? Escolha um número par entre 4 e 14 cartas"));
+}
 
-function numeroDeCartas(){
-    
-    let n = Number(jogo);
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+const cartas = [];
+
+function cartasEmbaralhadas(){
     let cont = 1;
-    while (cont<=n){
-        document.querySelector(`.container div:nth-child(${cont}`).classList.remove("escondido");
+    while (cont<=jogo){
+        cartas.push(document.querySelector(`.container div:nth-child(${cont}`));
         cont = cont + 1;
     }
+
+    for (let i=0; i<cartas.length; i++){
+        cartas[i].classList.remove("escondido");
+    }
 }
-numeroDeCartas()
+cartasEmbaralhadas()
