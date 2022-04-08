0b41f829d424e2ea5a4544b514b70d8b00947b9b
brandslucas@gmail.com <brandslucas@gmail.com>
2022-04-08 15:06:52 -0300

    Pares corretos fixados, pares incorreros desvirando.Problemas com contador de jogadas e tempo de ação do jogo

--- scripts.js
+++ scripts.js
@@ -9,59 +9,59 @@ function comparador() {
 }
 
 const cartasParaAparecer = [
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/bobrossparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/bobrossparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/explodyparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/explodyparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/fiestaparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/fiestaparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/metalparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/metalparrot.gif">
 </div>`, 
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/revertitparrot.gif">
 </div>`, 
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/revertitparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/tripletsparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/tripletsparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/unicornparrot.gif">
 </div>`,
-`<div class="imagens">
+`<div class="imagens" onclick="virarCarta(this)">
     <img class="frente" src="img/front.png">
     <img class="verso" src="img/unicornparrot.gif">
 </div>`]
@@ -84,8 +84,48 @@ function cartasEmbaralhadas(){
 }
 cartasEmbaralhadas()
 
-function virarCarta() {
-    
-  this.classList.toggle('virar');
+const carta = document.querySelectorAll(".imagens")
+let contadorJogadas=0;
+
+//varíável para guardar se existe ou não cartas viradas//
+let temCartaVirada = false;
+
+//variáveis para guardar qual carta foi virada primeiro e segundo//
+let primeiraCarta;
+let segundaCarta;
+
+//função que vai verificar se existe carta virada ou nãoo e atribuir os valores as variaveis primeira e segundCarta//
+function virarCarta(carta) {
+
+  carta.classList.add('virar');
+
+  if (temCartaVirada === false){
+      temCartaVirada=true;
+      primeiraCarta=carta;
+  }else{
+      segundaCarta=carta;
+      temCartaVirada=false;//ao final do bloco, independende do que acontecer, essa variável volta a ser false para uma p´roxima jogada//
+  }
+  
+  setTimeout(verificaçãoDasCartas, 5000);
+}
+function verificaçãoDasCartas(){
+    if (primeiraCarta.innerHTML===segundaCarta.innerHTML){
+        //remover o onclick dessas cartas//
+        
+        primeiraCarta.removeEventListener('click', virarCarta);
+        segundaCarta.removeEventListener('click', virarCarta);
+
+        contadorJogadas++//contar uma jogada - um par de cartas//
+        console.log(contadorJogadas);
+    }else{
+        setTimeout(desvirarCartas, 1000);
+        contadorJogadas++//contar uma jogada - um par de cartas//
+        console.log(contadorJogadas);
+    }
 }
 
+function desvirarCartas(){
+    primeiraCarta.classList.remove('virar');
+    segundaCarta.classList.remove('virar');
+}
