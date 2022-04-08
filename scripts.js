e12f33d7224cccd0d995e3b52b7952efd1e76389
brandslucas@gmail.com <brandslucas@gmail.com>
2022-04-08 10:26:24 -0300

    Cartas embaralhando, com backface hidden, hover (virar carta funcionando)

--- scripts.js
+++ scripts.js
@@ -8,17 +8,84 @@ function comparador() {
 	return Math.random() - 0.5; 
 }
 
-const cartas = [];
+const cartasParaAparecer = [
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/bobrossparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/bobrossparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/explodyparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/explodyparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/fiestaparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/fiestaparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/metalparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/metalparrot.gif">
+</div>`, 
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/revertitparrot.gif">
+</div>`, 
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/revertitparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/tripletsparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/tripletsparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/unicornparrot.gif">
+</div>`,
+`<div class="imagens">
+    <img class="frente" src="img/front.png">
+    <img class="verso" src="img/unicornparrot.gif">
+</div>`]
+
+const cartas = document.querySelector(".container");
 
 function cartasEmbaralhadas(){
-    let cont = 1;
-    while (cont<=jogo){
-        cartas.push(document.querySelector(`.container div:nth-child(${cont}`));
-        cont = cont + 1;
-    }
 
-    for (let i=0; i<cartas.length; i++){
-        cartas[i].classList.remove("escondido");
+    cartas.innerHTML="";
+
+    let embaralhadas = [];
+
+    for(let i=0; i<jogo; i++){
+        embaralhadas.push(cartasParaAparecer[i]);
+    }
+    embaralhadas.sort(comparador);
+    for (let j=0; j<embaralhadas.length; j++){
+        cartas.innerHTML+=`${embaralhadas[j]}`;
     }
 }
 cartasEmbaralhadas()
+
+function virarCarta() {
+    
+  this.classList.toggle('virar');
+}
+
